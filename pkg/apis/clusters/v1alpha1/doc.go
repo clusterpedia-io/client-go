634dc3d379fdf3e9743594f202bbfd5557513e32
calvin <wen.chen@daocloud.io>
2022-02-18 13:19:15 +0800

    move v1alpha1 to v1alpha2.
    Signed-off-by: calvin <wen.chen@daocloud.io>

--- pkg/apis/clusters/v1alpha1/doc.go
+++
@@ -1,7 +0,0 @@
-// +k8s:deepcopy-gen=package
-// +groupName=clusters.clusterpedia.io
-
-// Package v1alpha1 is the v1alpha1 version of the API
-package v1alpha1
-
-// TODO: if the apis package of clusterpedia is separated, the file will be remove.
